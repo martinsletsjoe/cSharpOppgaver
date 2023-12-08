1bb59a916ebffb487d107be6a7679f3493e726c2
martinsletsjoe <106916526+martinsletsjoe@users.noreply.github.com>
2023-12-08 15:01:01 +0100

    a

---
+++ 324A/324A/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _324A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+
+
+
+
+
+
+            /*
+             * 1. Use case diagram = hva er funksjonaliteten?
+             * 2. Klassediagram = hvem er skuespillerne? aktørene?
+             * 3. Sekvensdiagram for hvert use case
+             *          Hvordan samarbeider klassene for å oppnå hver bit funksjonalitet?
+             */
+        }
+    }
+}
